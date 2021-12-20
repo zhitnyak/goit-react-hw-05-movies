9f6ef31457075151587510c20588391582c75e73
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-20 22:20:36 +0200

    add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+// import Navigation from "./Navigation/Navigation";
+import { lazy, Suspense } from "react";
+import NavBar from "./NavBar/NavBar";
+import Container from "./Container/Container";
+import Spiner from "./Loader/Loader";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Container>
+        <NavBar />
+        <Suspense fallback={<Spiner />}>
+          {/* <Switch>
+            <Route path="/" exact>
+              <HomeView />
+            </Route>
+
+            <Route path="/movies">
+              <MovieView />
+            </Route>
+
+            <Route path="/movies/:movieId" exact>
+              <MovieDetailsView />
+            </Route>
+
+            <Redirect to="/" />
+          </Switch> */}
+        </Suspense>
+      </Container>
     </div>
   );
 }
